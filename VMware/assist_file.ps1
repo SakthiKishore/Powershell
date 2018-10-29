8a513618418decbeca813d01ec3595ebdf323438
Sakthi Kishore <31217771+SakthiKishore@users.noreply.github.com>
2018-10-29 11:34:01 +0530

    Create assist_file.ps1

---
+++ VMware/assist_file.ps1
@@ -0,0 +1,7 @@
+Connect-VIServer -Server 10.192.34.201
+
+Get-VM
+
+$ovfPath = "https://10.192.34.201/folder?dcPath=ha-datacenter&dsName=datastore1"
+
+$ovfConfig = Get-OvfConfiguration -Ovf $ovfPath
