d9b56727340c92d3333c4a5695c4ad09ef50cd9a
Sakthi Kishore <31217771+SakthiKishore@users.noreply.github.com>
2018-10-29 11:34:31 +0530

    Update assist_file.ps1

--- VMware/assist_file.ps1
+++ VMware/assist_file.ps1
@@ -5,3 +5,13 @@ Get-VM
 $ovfPath = "https://10.192.34.201/folder?dcPath=ha-datacenter&dsName=datastore1"
 
 $ovfConfig = Get-OvfConfiguration -Ovf $ovfPath
+
+
+
+
+function createvm
+{
+
+
+
+https://10.192.34.201/folder?dcPath=ha-datacenter&dsName=datastore1
