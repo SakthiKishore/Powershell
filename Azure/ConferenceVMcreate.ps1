e1c9d6df388d39507921a244b3fa3e3cf8ea582d
Sakthi Kishore <31217771+SakthiKishore@users.noreply.github.com>
2020-08-15 13:17:28 +0530

    Create ConferenceVMcreate.ps1

---
+++ Azure/ConferenceVMcreate.ps1
@@ -0,0 +1,10 @@
+param([string]$resourceGroup)
+
+$adminCredential = Get-Credential -Message "Enter a username and password"
+
+For ($i = 1, $i -le 3, $i++)
+{
+  $vmName = "ConferenceDemo" + $i
+  Write-Host "Creating VM:" $vmName 
+  New-AzVM -ResourceGroupName $resourceGroup -Name $vmName -Credential $adminCredential -Image UbuntuLTS
+}
